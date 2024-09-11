62caddafc451ddb8a6d3d99c9bfa3e10e7401e2e
sriyakasaudhan <kasaudhansriya@gmail.com>
2024-09-11 10:00:41 +0545

    Add files via upload

---
+++ Simple Calculator/script.js
@@ -0,0 +1,33 @@
+let input=document.getElementById('inputBox');
+//for selecting all buttons
+let buttons=document.querySelectorAll('button');
+
+//to call the buttons
+let string="";
+//making array foer passing the buttons
+let arr=Array.from(buttons);
+
+arr.forEach(button =>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string=eval(string);
+            input.value=string;
+        }
+
+        else if(e.target.innerHTML== 'AC'){
+            string="";
+            input.value=string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        }
+
+        
+        else{
+            string+=e.target.innerHTML;
+            input.value=string;
+        }
+
+    })
+})
